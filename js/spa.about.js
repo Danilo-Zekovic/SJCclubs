7f291a486d02a5b83152e86dbd1ae8da3420c78b
Danilo Zekovic <dzv9776@saintjoe.edu>
2016-01-30 22:01:01 -0500

    minor changes

--- js/spa.about.js
+++ js/spa.about.js
@@ -18,7 +18,7 @@ spa.about = (function (){
         +   '</div>'
         +   '<div class="row">'
         +     '<div class="col-md-12"'
-        +       '<p>text goes here</p>'
+        +       '<p>When looking for a club or activity that interests you, your biggest challenge will be deciding which one to get involved with first. Travel to North Carolina with Habitat for Humanity, plan events like our annual Little 500 as a member of the Puma Activities Council (PAC), or test your mettle with the Rugby Club. Because of our small student population, you can easily take a leadership role in whatever club or activity you get involved with. And if we don’t have what you’re looking for, you can always start it yourself.</p>'
         +     '</div>'
         +   '</div>'
         + '</div>'
